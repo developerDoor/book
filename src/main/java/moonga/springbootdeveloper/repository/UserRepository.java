269de8cf3cf57f33f0095b5d94aea794a8f10b62
jhmoon <dothebestmoon1016@gmail.com>
2023-09-20 16:46:25 +0900

    스프링 시큐리티 시작

---
+++ src/main/java/moonga/springbootdeveloper/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package moonga.springbootdeveloper.repository;
+
+import moonga.springbootdeveloper.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String emial); // email로 사용자 정보를 가져옴
+}
